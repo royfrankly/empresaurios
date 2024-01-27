9ee9558719ac69eec98806c12d67d243d8d0f266
rofrankly <ryfrnkl_nvrr@outlook.es>
2024-01-27 12:08:29 -0500

    conh add class contacto

--- contacto.h
+++ contacto.h
@@ -1,6 +1,24 @@
 #ifndef CONTACTO_H
 #define CONTACTO_H
-
-
-
+#include <iostream>
+using namespace std;
+class Contacto{
+    protected:
+        string direccion;
+        string correo;
+        string telefono;
+        string sitioWeb;
+    public:
+        Contacto();
+        Contacto(string, string, string, string);
+        string getDireccion();
+        void setDireccion(string);
+        string getCorreo();
+        void setCorreo(string);
+        string getTelefono();
+        void setTelefono(string);
+        string getSitioWeb();
+        void setSitioWeb(string);
+        void mostrar();
+};
 #endif
