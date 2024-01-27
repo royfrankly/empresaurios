cd89c8ea7c7849eb558d5e8401fdaea0ac624fbc
NicolasEMelendez <nemelendezg@unjbg.edu.pe>
2024-01-27 11:53:28 -0500

    tarea.cpp

---
+++ tarea.cpp
@@ -0,0 +1,15 @@
+#include "tarea.h"
+
+//constructores----------------------------------------
+Tarea::Tarea(){
+    descripcionTarea = "";
+    estadoTarea = "";
+}
+Tarea::Tarea(string _descripcionTarea, string _estadoTarea){
+    descripcionTarea = _descripcionTarea;
+    estadoTarea = _estadoTarea;
+}
+//metodos----------------------------------------------
+void Tarea::cambiarEstado(){
+    
+}
