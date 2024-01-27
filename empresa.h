f243c0adf51d352fa7777942cd35358710f5bf57
NicolasEMelendez <nemelendezg@unjbg.edu.pe>
2024-01-27 10:56:48 -0500

    add empresa.h

---
+++ empresa.h
@@ -0,0 +1,28 @@
+#ifndef EMPRESA_H
+#define EMPRESA_H
+#include "contacto.h"
+#include "empleado.h"
+#include "proyecto.h"
+#include <bits/stdc++.h>
+using namespace std;
+
+class Empresa{
+    private:
+        string nombre;
+        string ruc;
+        string descripcion;
+        contacto contactoempresa;
+        empleado listaempleados[100];
+        proyecto listaproyectos[100];
+    public:
+        Empresa();
+        Empresa(string, string, string, contacto, empleado, proyecto);
+        void mostrarEmpresa();
+        void contratarEmpleado();
+        void despedirEmpleado();
+        void crearProyecto();
+        void crearProyecto();
+        void agregarProyecto();
+};
+
+#endif
