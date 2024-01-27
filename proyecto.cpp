6ff158849f8496df75a14e68b7ce7d484bec832c
rofrankly <ryfrnkl_nvrr@outlook.es>
2024-01-27 13:38:06 -0500

    proc create y add methods proyecto

---
+++ proyecto.cpp
@@ -0,0 +1,21 @@
+#include "proyecto.h"
+#include "tarea.h"
+Proyecto::Proyecto(){
+    fechaCreacion = "";
+    fechaFin = "";
+}
+Proyecto::Proyecto(string _nombre, string _fechaCreacion, string _fechaFin, Equipo lEquipo, Tarea lTarea, string _descripcion, string _estado):Tarea( _descripcion, _estado){
+    nombre = _nombre;
+    fechaCreacion = _fechaCreacion;
+    fechaFin = _fechaFin;
+
+}
+Proyecto::~Proyecto(){
+
+}
+void Proyecto::crearTarea(){
+    
+}
+
+
+                                                               
