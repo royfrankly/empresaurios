7885d0207effdb2f00054799bbfc94e4be2a721f
rofrankly <ryfrnkl_nvrr@outlook.es>
2024-01-27 10:51:13 -0500

    add metodos

---
+++ equipo.cpp
@@ -0,0 +1,21 @@
+#include "equipo.h"
+//constructores--------------------------------
+Equipo::Equipo(){
+    id=0;
+    nombre="";
+}
+Equipo::Equipo(int _id, string _nombre){
+    id=_id;
+    nombre=_nombre;
+    
+}
+//metodos--------------------------------
+void Equipo::agregarMiembro(Empleado){
+
+}
+void Equipo::removerMiembro(Empleado){
+
+}
+void Equipo::agregarLider(Empleado){
+    
+}
