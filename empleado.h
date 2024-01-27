9ed88570dcb4578b0b87c72ae32c7fe09e522400
rofrankly <ryfrnkl_nvrr@outlook.es>
2024-01-27 10:50:37 -0500

    change metodos a su cpp

--- empleado.h
+++ empleado.h
@@ -25,55 +25,6 @@ class Empleado{
         void setSalario(float);
         void mostrarEmpleado();
 };
-//constructores--------------------------------
-Empleado::Empleado(){
-    id=0;
-    nombre="";
-    tipo="";
-    salario=0;
-}
-Empleado::Empleado(int _id, string _nombre, string _tipo, float _salario){
-    id=_id;
-    nombre=_nombre;
-    tipo=_tipo;
-    salario=_salario;
-}
-//destructor--------------------------------
-Empleado::~Empleado(){
-    
-}
-int Empleado::getId(){
-    return id;
-}
-void Empleado::serId(int nuevoId){
-    id=nuevoId;
-}
-string Empleado::getNombre(){
-    return nombre;
-}
-void Empleado::setNombre(string nuevoNombre){
-    nombre=nuevoNombre;
-}
-string Empleado::getTipo(){
-    return tipo;
-}
-void Empleado::setTipo(string nTipo){
-    tipo=nTipo;
-}
-float Empleado::getSalario(){
-    return salario;
-}
-void Empleado::setSalario(float nSalario){
-    salario=nSalario;
-}
-void Empleado::mostrarEmpleado(){
-    cout<<"Datos del empleado:"<<endl;
-    cout<<"Nombre: "<<nombre<<endl;    
-    cout<<"Id: "<<id<<endl;
-    cout<<"Tipo: "<<tipo<<endl;
-    cout<<"Salario: "<<salario<<endl;
-}
-
 #endif
 
 
