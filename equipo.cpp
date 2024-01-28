8fc5b1f314c26302663b0e9e71148d9fe62e7314
royfrankly <ryfrnkl_nvrr@outlook.es>
2024-01-28 06:23:38 -0500

    equc add atributo numMiembros al constructor

--- equipo.cpp
+++ equipo.cpp
@@ -3,11 +3,12 @@
 Equipo::Equipo(){
     id=0;
     nombre="";
+    numMiembros=0;
 }
-Equipo::Equipo(int _id, string _nombre){
+Equipo::Equipo(int _id, string _nombre,int _numMiembros){
     id=_id;
     nombre=_nombre;
-    
+    numMiembros=_numMiembros;
 }
 //metodos--------------------------------
 void Equipo::agregarMiembro(Empleado){
