03215a125c52678bf191b7ff1ec67ea8d60b5ae2
royfrankly <ryfrnkl_nvrr@outlook.es>
2024-01-28 06:43:42 -0500

    equc add methods get,set lider

--- equipo.cpp
+++ equipo.cpp
@@ -11,12 +11,44 @@ Equipo::Equipo(int _id, string _nombre,int _numMiembros){
     numMiembros=_numMiembros;
 }
 //metodos--------------------------------
-void Equipo::agregarMiembro(Empleado){
-
+int Equipo::getId(){
+    return id;
 }
-void Equipo::removerMiembro(Empleado){
-
+void Equipo::setId(int nuevoId){
+    id=nuevoId;
 }
-void Equipo::agregarLider(Empleado){
+string Equipo::getNombre(){
+    return nombre;
+}
+void Equipo::setNombre(string nuevoNombre){
+    nombre=nuevoNombre;
+}
+int Equipo::getNumMiembros(){
+    return numMiembros;
+}
+Empleado Equipo::getLider(){
+    return lider;
+}
+void Equipo::setLider(Empleado nuevoLider){
+    lider=nuevoLider;
+}
+void Equipo::setNumMiembros(int nuevoNumMiembros){
+    numMiembros = nuevoNumMiembros;
+}
+void Equipo::agregarMiembro(Empleado empleMiembro){
+    if (numMiembros<100){
+        listaMiembros[numMiembros++] = empleMiembro;
+        
+        cout<<"Nuevo empleado contratado."<<endl;
+        
+    }
+    else{
+        cout<<"No se pueden contratar más empleados"<<endl;
     
+}
+//void Equipo::removeMiembro(Empleado empleMiembro){
+//
+//}
+void Equipo::agregarLider(Empleado empleMiembro){
+    lider = empleMiembro;
 }
