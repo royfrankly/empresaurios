1440f8b8be0caa6fc376d70109de09e986d748d7
royfrankly <ryfrnkl_nvrr@outlook.es>
2024-01-30 17:06:22 -0500

    tarh add sobrecargas < y >

--- tarea.h
+++ tarea.h
@@ -7,13 +7,19 @@ class Tarea{
         string descripcion;
         string estado;
     public:
+    //-----------constructores-----------------------------------------------------
         Tarea();
         Tarea(string, string);
+    //-----------geters y seters----------------------------------------------------
         string getDescripcion();
         void setDescripcion(string);
         string getEstado();
         void setEstado(string);
+    //------------mostrar---------------------------------------------------
         void mostrar();
+    //------------sobrecargas----------------------------------------------------
+        bool operator <(Tarea&);
+        bool operator >(Tarea&);
         friend ostream& operator<<(ostream& , const Tarea& );
         friend istream& operator>>(istream&, Tarea&);
 };
