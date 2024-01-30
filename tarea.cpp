1262ce399410d96563ae6515e6836c95fc14c43f
royfrankly <ryfrnkl_nvrr@outlook.es>
2024-01-30 18:41:13 -0500

    tarh add method leerElArchivo

--- tarea.cpp
+++ tarea.cpp
@@ -2,6 +2,7 @@
 #include <iostream>
 #include <fstream>
 #include <sstream>
+#include <iomanip>
 Tarea::Tarea(){
     descripcion = "";
     estado = "";
@@ -49,14 +50,42 @@ istream& operator>>(istream& is, Tarea& tarea) {
     getline(is, tarea.estado);
     return is;
 }
+//crea o guarda las tareas en el archivo
 void Tarea::guardarEnArchivo(string codigo) {
     string carpeta = "tareas/tarea";
     string nombreArchivo = carpeta + codigo + ".txt";
-    ofstream archivoSalida(nombreArchivo);
+    ofstream archivoSalida(nombreArchivo, ios::app);
     if (archivoSalida.is_open()) {
-        archivoSalida << *this;
+        archivoSalida << *this<<endl;
         archivoSalida.close();
         cout << "Tarea almacenada en el archivo: " << nombreArchivo << endl;
+    } else {
+        cout << "No se pudo abrir el archivo para escritura." << endl;
+    }
+}
+//lee el archivo
+void Tarea::leerElArchivo(string codigo) {
+    string est, des;
+    char aux1, aux2,aux3;
+    string carpeta = "tareas/tarea";
+    string nombreArchivo = carpeta + codigo + ".txt";
+    ifstream archivoSalida(nombreArchivo);
+    if (archivoSalida.is_open()) {
+        cout<<"LISTA DE TAREAS DEL EMPELADO CON ID"<<codigo<<"\n\n";
+        //prepara encabezado de tabla a mostrar
+        cout<<left;
+        cout<<setw(10)<<"Estado"<<setw(17)<<"Descripcion"<<setw(50);
+        while(archivoSalida.eof()) {
+            archivoSalida >> est;
+            archivoSalida.get(aux1);
+            archivoSalida.get(aux2);
+            archivoSalida.get(aux3);
+            getline(archivoSalida, des);
+            cout << est<<": "<<des<<endl;
+        }
+        
+        archivoSalida.close();
+        
     } else {
         cout << "No se pudo abrir el archivo para escritura." << endl;
     }
