1c639132e8dcd87fa7e00db6f180e912bb769cdd
royfrankly <ryfrnkl_nvrr@outlook.es>
2024-01-30 18:45:28 -0500

    tarc coment method leerElArchivo

--- tarea.cpp
+++ tarea.cpp
@@ -64,14 +64,14 @@ void Tarea::guardarEnArchivo(string codigo) {
     }
 }
 //lee el archivo
-void Tarea::leerElArchivo(string codigo) {
+/*void Tarea::leerElArchivo(string codigo) {
     string est, des;
     char aux1, aux2,aux3;
     string carpeta = "tareas/tarea";
     string nombreArchivo = carpeta + codigo + ".txt";
     ifstream archivoSalida(nombreArchivo);
     if (archivoSalida.is_open()) {
-        cout<<"LISTA DE TAREAS DEL EMPELADO CON ID"<<codigo<<"\n\n";
+        cout<<"LISTA DE TAREAS DEL EMPELADO CON ID "<<codigo<<"\n\n";
         //prepara encabezado de tabla a mostrar
         cout<<left;
         cout<<setw(10)<<"Estado"<<setw(17)<<"Descripcion"<<setw(50);
@@ -89,4 +89,4 @@ void Tarea::leerElArchivo(string codigo) {
     } else {
         cout << "No se pudo abrir el archivo para escritura." << endl;
     }
-}
+}*/
